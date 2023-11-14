855acd23cb802a1e5a015877a35f91358f4ba7fe
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-11-14 16:36:46 -0500

    Implement create room endpoint

--- src/azure/dto/create-azure.dto.ts
+++ src/azure/dto/create-azure.dto.ts
@@ -1,42 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class CreateACSTokenDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   public scope: string;
 }
 
 export class CreateChatThreadDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   public topicName: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   public displayName: string;
 }
 
 export class AddUserChatThreadDto {
+  @ApiProperty()
   @IsString()
   public userId: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   public adminUserId: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   public displayName: string;
 }
 
 export class UserConfigDto {
+  @ApiProperty()
   @IsString()
   emoji: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   displayName: string;
 
+  @ApiProperty()
   @IsString()
   threadId: string;
 }
